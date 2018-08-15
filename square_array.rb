bf34df587424ddd5561bd55891bfaa9d2a6b2120
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-15 22:13:28 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,10 @@
 def square_array(array)
 
   square_array_of_nums =[]
+  
   array.each do |value_to_square|
 
-    value_to_square * value_to_square
-    
-    square_array_of_nums << 
+    square_array_of_nums << value_to_square * value_to_square
     
 end
   square_array_of_nums
