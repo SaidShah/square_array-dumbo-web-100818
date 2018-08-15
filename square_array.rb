60fa80e8ec60446194f4084c40d9c7d5d302843f
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-15 22:08:43 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,11 @@
 def square_array(array)
   # your code here
   square_array_of_nums =[]
-  array.each do |nums|
+  array.each do |value_to_square|
+    
     squared_num = (nums*nums)
     square_array_of_nums << squared_num
+    
 end
   
 end
